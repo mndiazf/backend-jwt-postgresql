4846a7c8cc52cec6ba638ca692ec1e7b76d93ff1
mdiazpj <mn.diaz.f@gmail.com>
2024-07-05 16:20:55 -0400

    trying

--- src/main/java/com/backend/BackendJWT/Controllers/Shop/ShopController.java
+++ src/main/java/com/backend/BackendJWT/Controllers/Shop/ShopController.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "https://mousecat.lol", allowedHeaders = "*")
 @RequestMapping("/shop/productos")
 public class ShopController {
 
